b0230cef218c364d1c6f72dd5910e5db968f9ff7
MyTran2508 <mytranchi2508@mail.com>
2023-07-07 14:02:45 +0700

    mytc update CorsConfig

--- src/main/java/com/ocena/qlsc/common/util/CorsConfig.java
+++ src/main/java/com/ocena/qlsc/common/util/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.1.67:5173")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
