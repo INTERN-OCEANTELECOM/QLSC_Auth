50044fdb88417b1c5d79b9540ab353c5d82ef009
phamngocthang <106516350+phamngocthang@users.noreply.github.com>
2023-07-10 11:51:39 +0700

    thangpn create update repairCategory & bbbgnumber podetail

--- src/main/java/com/ocena/qlsc/podetail/status/RegexConstants.java
+++ src/main/java/com/ocena/qlsc/podetail/status/RegexConstants.java
@@ -3,13 +3,13 @@ package com.ocena.qlsc.podetail.status;
 import java.util.*;
 
 public final class RegexConstants {
-    public static final String regexRepairCategory = "(?i)\\s*H(?:[ẠA]|\\p{L})NG\\s*M(?:[ỤU]|\\p{L})C\\s*";
+    public static final String regexRepairCategory = "(?i)\\s*H(?:[ẠA]|\\p{L})NG\\s*M(?:[ỤU]|\\p{L})C\\s*SC\\s*";
 
     public static final String regexSTT = "(?i)\\s*STT\\s*";
 
-    public static final String regexSerialNumber = "(?i)\\s*S(?:[ỐÔÔ]|\\p{L})\\s*SERIAL\\s*H(?:[ỎO]|\\p{L})NG\\s*";
+    public static final String regexSerialNumber = "(?i)\\s*S(?:[ỐÔÔ]|\\p{L})\\s*SERIAL\\s*";
 
-    public static final String regexProduct = "(?i)\\s*M(?:[ÃA]|\\p{L})\\s*HH\\s*";
+    public static final String regexProduct = "(?i)\\s*M(?:[ÃA]|\\p{L})\\s*H(?:[ÀA]|\\p{L})NG\\s*H(?:[Ó]|\\p{L})A\\s*";
 
     public static final String regexPo = "(?i)\\s*S(?:[ỐÔO]|\\p{L})\\s*PO\\s*";
 
@@ -17,17 +17,17 @@ public final class RegexConstants {
 
     public static final String regexImportDate = "(?i)\\s*NG(?:[ÀA]|\\p{L})Y\\s*NH(?:[ÂAẬ]|\\p{L})P\\s*";
 
-    public static final String regexRepairStatus = "(?i)\\s*TR(?:[ẠA]|\\p{L})NG\\s*TH(?:[ÁA]|\\p{L})I\\s*SC\\s*";
+    public static final String regexRepairStatus = "(?i)\\s*C(?:[ẬAÂ]|\\p{L})P\\s*NH(?:[ÂAẬ]|\\p{L})T\\s*SC\\s*";
 
     public static final String regexProductName = "(?i)\\s*T(?:[ÊE]|\\p{L})N\\s*THI(?:[ÊEẾ]|\\p{L})T\\s*B(?:[ỊI]|\\p{L})\\s*";
 
-    public static final String regexExportPartner = "(?i)\\s*XU(?:[ÂAẤ]|\\p{L})T\\s*kho\\s*tr(?:[ảa]|\\p{L})\\s*kh\\s*";
+    public static final String regexExportPartner = "(?i)\\s*C(?:[ẬAÂ]|\\p{L})P\\s*NH(?:[ÂAẬ]|\\p{L})T\\s*XK\\s*";
 
-    public static final String regexKcsVT = "(?i)\\s*KSC\\s*VT\\s*";
+    public static final String regexKcsVT = "(?i)\\s*C(?:[ẬAÂ]|\\p{L})P\\s*NH(?:[ÂAẬ]|\\p{L})T\\s*KCS\\s*";
 
-    public static final String regexWarrantyPeriod = "(?i)\\s*B(?:[AẢ]|\\p{L})O\\s*H(?:[AÀ]|\\p{L})NH\\s*";
+    public static final String regexWarrantyPeriod = "(?i)\\s*C(?:[ẬAÂ]|\\p{L})P\\s*NH(?:[ÂAẬ]|\\p{L})T\\s*BH\\s*";
 
-    public static final String regexPriority = "(?i)\\s*(?:ƯU]|\\p{L})U\\s*TI(?:[EÊ]|\\p{L})N\\s*";
+    public static final String regexPriority = "(?i)\\s*(?:ƯU]|\\p{L})U\\s*TI(?:[EÊ]|\\p{L})N\\s*SC\\s*";
 
     public static final HashMap<Integer, String> importPOHeader = new HashMap<>() {
         {
@@ -111,7 +111,15 @@ public final class RegexConstants {
         }
     };
 
-
+    public static final HashMap<Integer, String> bbbgNumberMap = new HashMap<>() {
+        {
+            put(0, regexSTT);
+            put(1, regexProduct);
+            put(2, regexSerialNumber);
+            put(3, regexPo);
+            put(4, regexBbbgNumber);
+        }
+    };
 
     public static final HashMap<Integer, String> importProduct = new HashMap<>() {
         {
